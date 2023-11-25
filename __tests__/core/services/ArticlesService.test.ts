467448240eafb13ef38ad35d66cbab2d4b710d7e
LoicZHU <zhu.loic@gmail.com>
2023-11-25 16:17:47 +0100

    fix: config, add jest config, add unit tes

---
+++ __tests__/core/services/ArticlesService.test.ts
@@ -0,0 +1,12 @@
+import {getArticles} from "../../../src/core/articles/services/ArticlesService";
+
+describe('ArticlesService', () => {
+  it('should be false', () => {
+    expect(false).toBe(false);
+  });
+
+  it('getArticles should not be equal to false', async () => {
+    // expect(await getArticles()).not.toBe(false);
+    expect(await getArticles).not.toEqual(false);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "env",
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin('all')],
 });
