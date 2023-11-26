674f66e34fdca6ef8b51df0c94a02acaf0136bf9
LoicZHU <zhu.loic@gmail.com>
2023-11-26 13:16:25 +0100

    comment new 'bad-code' index

--- src/bad-code/index.ts
+++ src/bad-code/index.ts
@@ -1,40 +1,8 @@
-function calculateArea(radius: number): number {
-  const pi = 3.14159;
-  return pi * radius * radius;
-}
-
-function calculateCircumference(radius: number): number {
-  const pi = 3.14159;
-  return 2 * pi * radius;
-}
-// function calculateCircumference(radius: number): number {
+// function calculateArea(radius: number): number {
 //   const pi = 3.14159;
-//   return 2 * pi * radius;
+//   return pi * radius * radius;
 // }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
-// function calculateCircumference(radius: number): number {
-//   const pi = 3.14159;
-//   return 2 * pi * radius;
-// }
+//
 // function calculateCircumference(radius: number): number {
 //   const pi = 3.14159;
 //   return 2 * pi * radius;
@@ -44,10 +12,10 @@ function calculateCircumference(radius: number): number {
 //   return 2 * pi * radius;
 // }
 
-(function runBadCodeForSonarCloud(): void {
-  console.log("Bad code for SonarCloud: ");
-  console.log(`calculated area with a radius of 3: ${calculateArea(3)}`);
-  console.log(
-    `calculated circumference with a radius of 3: ${calculateCircumference(3)}`,
-  );
-})();
+// (function runBadCodeForSonarCloud(): void {
+//   console.log("Bad code for SonarCloud: ");
+//   console.log(`calculated area with a radius of 3: ${calculateArea(3)}`);
+//   console.log(
+//     `calculated circumference with a radius of 3: ${calculateCircumference(3)}`,
+//   );
+// })();
