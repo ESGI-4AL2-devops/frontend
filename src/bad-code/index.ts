df140d94d2aa234e045c8ca7ed9f67c4f65c494b
LoicZHU <zhu.loic@gmail.com>
2023-11-26 09:40:37 +0100

    add bad code

---
+++ src/bad-code/index.ts
@@ -0,0 +1,17 @@
+function calculateArea(radius) {
+  const pi = 3.14159;
+  return pi * radius * radius;
+}
+
+function calculateCircumference(radius) {
+  const pi = 3.14159;
+  return 2 * pi * radius;
+}
+
+(function runBadCodeForSonarCloud(): void {
+  console.log("Bad code for SonarCloud: ");
+  console.log(`calculated area with a radius of 3: ${calculateArea(3)}`);
+  console.log(
+    `calculated circumference with a radius of 3: ${calculateCircumference(3)}`,
+  );
+})();
