085f3239d5146a8acaf4b4bf7473edafed243f4e
LoicZHU <zhu.loic@gmail.com>
2023-12-21 15:15:48 +0100

    fix

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -1,6 +1,6 @@
 const CONFIGURATION = {
-  // API_URL: import.meta.env.VITE_API_URL,
-  API_URL: process.env.VITE_API_URL,
+  API_URL: import.meta.env.VITE_API_URL,
+  // API_URL: process.env.VITE_API_URL,
 };
 
 export default CONFIGURATION;
